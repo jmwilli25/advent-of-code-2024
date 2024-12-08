273054a9f1b145031b4d3a0c6d9dedfdaaafa111
jmwilli25 <jmwilli25@gmail.com>
2024-12-08 12:54:37 -0500

    Day 1 complete

--- day1/part1.py
+++ day1/part1.py
@@ -1,18 +1,17 @@
 left_array = []
 right_array = []
 
-with open("artifacts/day1.0.1.txt", "r") as f:
+with open("artifacts/day1.0.0.txt", "r") as f:
     for line in f:
-        left, right = line.split(" ")
+        left, right = line.split(",")
         left_array.append(int(left))
         right_array.append(int(right))
 
-# sort the arrays
 left_array.sort()
 right_array.sort()
 
 # get the difference between the two arrays
-diff = [left - right for left, right in zip(left_array, right_array)]
+diff = [abs(left - right) for left, right in zip(left_array, right_array)]
 
 # sum the difference in the diff list
 total = sum(diff)
