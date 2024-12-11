23c0b3c037de5a0ebe5947135220a865c1224ad8
jmwilli25 <jmwilli25@gmail.com>
2024-12-10 21:37:37 -0500

    day 2 part 1 is complete. Added a test case in day2.sample.txt for all reports being identical

---
+++ day2/part1.py
@@ -0,0 +1,29 @@
+reports_array = []
+
+def convert_to_int(array: list) -> list:
+    return [int(item) for item in array]
+
+def report_distance_within_range(report_1: int, report_2: int) -> bool:
+    report_distance = abs(report_1 - report_2)
+    return 1 <= report_distance <= 3
+
+with open("artifacts/day2.full.txt", "r") as f:
+    for report in f:
+        reports_array.append(convert_to_int(report.strip("\n").split(" ")))
+
+safe_count = 0
+for report in reports_array:
+    direction_array = []
+    for i in range(0, len(report) - 1):
+        adjacent_levels_within_tolerance = report_distance_within_range(report[i], report[i + 1])
+        if report[i] > report[i + 1] and adjacent_levels_within_tolerance:
+            direction_array.append("descending")
+        elif report[i] < report[i + 1] and adjacent_levels_within_tolerance:
+            direction_array.append("ascending")
+        else:
+            direction_array.append("level")
+    direction = set(direction_array)
+    if len(direction) == 1 and "level" not in direction:
+        safe_count += 1
+
+print(safe_count)
